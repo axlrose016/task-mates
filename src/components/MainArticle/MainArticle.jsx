ab9e3eef00e79e67ff0794aaac49e5914456b0b9
Axl Rose Villanueva <axlrose.aj.villanueva.016@gmail.com>
2023-08-14 22:23:47 +0800

    Deployment to Vercel

--- src/components/MainArticle/MainArticle.jsx
+++ src/components/MainArticle/MainArticle.jsx
@@ -12,18 +12,18 @@ function MainArticle() {
   return (
     <div className='main-article-container' id="home">
         <video src="/videos/video-2.mp4" autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
-        <p>What are you waiting for?</p>
+        <h1>[Main Article]</h1>
+        <p>[Sub Article]</p>
         <div className='main-article-btns'>
             <Button className='btns' 
                 buttonStyle='btn--outline'
                 buttonSize='btn--large'>
-                    GET STARTED
+                    Button 1
             </Button>
             <Button className='btns'
                 buttonStyle='btn-primary'
                 buttonSize='btn--large'>
-                    WATCH TRAILER <FontAwesomeIcon icon='fas fa-bars'/>
+                    Button 2 <FontAwesomeIcon icon='fas fa-bars'/>
             </Button>
         </div>
     </div>
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -5,40 +5,40 @@ import ServicesCard from '../../components/ServicesCard/ServicesCard';
 function Services() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Check out our Services!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <ServicesCard
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              src='images/img-10.jpg'
+              text='The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.)'
               label='Adventure'
               path='/services'
             />
             <ServicesCard
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              src='images/img-11.jpg'
+              text='The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.'
+              label='Friends'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <ServicesCard
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              src='images/img-12.jpg'
+              text='The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.'
+              label='Collaborate'
               path='/services'
             />
             <ServicesCard
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              src='images/img-13.jpg'
+              text='One brave soul did take a stab at translating the almost-not-quite-Latin.'
+              label='Work'
               path='/products'
             />
             <ServicesCard
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              src='images/img-14.jpg'
+              text='Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa.'
+              label='Commitment'
               path='/sign-up'
             />
           </ul>
